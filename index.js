de6f66bbeec776c9ae39ea8a5af54f017ee37592
Dirk Arnez <smalldirkalex@gmail.com>
2025-08-11 20:21:16 +0800

    - add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function PrintingUnitsCalculator() {
+  const [ data, setData ] = React.useState({});
+
+  React.useEffect(() => {
+  }, []);
+
+  return (
+    <div>
+      <a href="https://github.com/dirkarnez/printing-units-calculator" target="_blank">
+        printing-units-calculator
+      </a>
+      <br/>
+
+    </div>
+  )
+}
